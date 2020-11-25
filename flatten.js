2a91f5613126408b499e719af3bb391d55456d60
Michael Xiang <yorkmike4@gmail.com>
2020-11-25 02:56:39 +0000

    flatten

---
+++ flatten.js
@@ -0,0 +1,21 @@
+let flatten = function(list) {
+  let output = [];
+  
+  for (let i = 0; i < list.length; i++) {
+  
+    if (Array.isArray(list[i])) {
+      let subList = list[i];
+      for (let k = 0; k < subList.length; k++) {
+  
+        output += subList[k];
+  
+      }
+  
+    } else {
+  
+      output += list[i];
+  
+    }
+  } return output;
+};
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
