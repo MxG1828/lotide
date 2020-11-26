7f0c266a416b16ae6df3da3445ec1ccd042e459d
Michael Xiang <yorkmike4@gmail.com>
2020-11-26 23:04:42 +0000

    adding takeUntill, and update everything

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,17 @@
 
 
-let eqArrays = function(array1,array2) {
+let assertArraysEqual = function(array1,array2) {
   if(array1.length !== array2.length) {
     console.log(`🛑🛑🛑Assertion Failed: ${array1} !== ${array2}`);
     return;
   } else {
-    for(let i = 0; i < array1.length; i++){
-      if(array1[i] !== array2[i]){
+    for(let i = 0; i < array1.length; i++) {
+      if(array1[i] !== array2[i]) {
         console.log(`🛑🛑🛑Assertion Failed: ${array1} !== ${array2}`);
         return;
       }
     }
   }console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`);
 }
-eqArrays([1,2,3,4],[1,2,4]);
+assertArraysEqual([1,2,3,4],[1,2,3,4]);
+assertArraysEqual([1,2,3,4],['1','2','3','4']);
